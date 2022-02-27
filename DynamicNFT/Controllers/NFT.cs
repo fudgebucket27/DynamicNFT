0990fe3c9b370977098bcd126d2ae4c161156b80
Fudgey <fudgebucket27@gmail.com>
2022-02-28 04:30:48 +1000

    nft sort of working

---
+++ DynamicNFT/Controllers/NFT.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DynamicNFT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NFT : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            var imageLocation = currentDirectory + @"\NFT\0.jpg";
+            var image = System.IO.File.OpenRead(imageLocation);
+            return File(image, "image/jpeg");
+        }
+    }
+}
