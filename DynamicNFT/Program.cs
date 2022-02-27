340475ac09facd8e9349932665d6d52c0a949184
Fudgey <fudgebucket27@gmail.com>
2022-02-28 05:36:14 +1000

    allow cors

--- DynamicNFT/Program.cs
+++ DynamicNFT/Program.cs
@@ -1,10 +1,26 @@
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      builder =>
+                      {
+                          builder.WithOrigins("https://loopring.io";
+                      });
+});
+
+
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 // Configure the HTTP request pipeline.
 
