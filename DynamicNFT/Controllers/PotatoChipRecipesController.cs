06e6b4e3d13f5ee9ca8c0884df45c0c4eb43fd18
John <john.trembath@glencore.com.au>
2022-03-04 14:59:17 +1000

    branch for potato chip recipes

---
+++ DynamicNFT/Controllers/PotatoChipRecipesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DynamicNFT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PotatoChipRecipesController : ControllerBase
+    {
+    }
+}
